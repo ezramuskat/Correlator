1bb1accf41166d2b1c4ff5d54d72a8efb4023145
Ezra Muskat <ezramuskat@gmail.com>
2022-11-17 23:23:52 -0500

    added initial database support

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def init_app():
 	"""Initialize the core application."""
 	app = Flask(__name__, instance_relative_config=False)
 	app.config.from_object('config.Config')
+	db.init_app(app)
+	login_manager.init_app(app)
 
 	with app.app_context():
 		# Include our Routes
--- server/models.py
+++ server/models.py
@@ -1,4 +1,36 @@
+from . import db
 
+
+class User(db.Model):
+    """Data model for user accounts."""
+
+    __tablename__ = 'pattern-finder-users'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    username = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    email = db.Column(
+        db.String(80),
+        index=True,
+        unique=True,
+        nullable=False
+    )
+    created = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=False
+    )
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 #this is going to be updated to be a db model later
 #likely going to change datapoints to reflect a graph
 class Pattern():
